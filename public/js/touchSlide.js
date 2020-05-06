d2c7a079ff9140d2da91ff2db3e3b1a2af11d313
juuuuunic <juuuuuniic@gmail.com>
2020-05-06 17:59:34 +0900

    200506_mail_send

--- public/js/touchSlide.js
+++ public/js/touchSlide.js
@@ -1,22 +1,38 @@
 $(document).ready(function() {
-    $('.slides').append( $('.slides > li').eq(0).clone() );
+    $(".slides").append(	$(".slide").eq(0).clone()	);
 });
-
+/* Slide 만드는 함수 */
+// function slideMaker($obj, isClone) {
+//     if(isClone) $(".slides").append(	$(".slide").eq(0).clone()	);
+// }
 
 //slide01
 (function(){
-    var  interval = setInterval(slide01, 3000) ;
-    var n = 1;
-   
+    
+	var $stage = $("#stage");
+    var $slides =  $stage.find(".slides");
+    var interval;
+	var speed = 500;
+	var delay = 1500;
+	var n = 0;
+    var last, $slide;
+
+    $slide = $slides.find(".slide");
+    last = $slide.length;
+    
+    /* 이벤트선언 */
+	var interval = setInterval(intervalSlide, delay);
+    
     function ani() {
-        $(".slides").stop().animate({"left":(-n*100)+"%"},500);
+        $slides.stop().animate({"left": -n*100+"%"}, speed);
 	}
-    function slide01() {
-        if(n == $('.slides > li').length -1) {
-            n = 0; 
-            $(".slides").css({"left": 0});
-        }
-        n++;
+    function intervalSlide() {
+		if(n == last - 1) {
+			n = 0;
+			$slides.css({"left": 0});
+		}
+		n++;
         ani();
-    };
+        console.log($stage)
+	}
 })();
